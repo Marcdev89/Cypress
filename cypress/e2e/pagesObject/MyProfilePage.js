b7185ebb01f24ef515359282b6b06b19ab0cad20
nebelorz <nebelorz@hotmail.com>
2023-03-20 13:24:12 +0100

    fix

--- cypress/e2e/pagesObject/MyProfilePage.js
+++ cypress/e2e/pagesObject/MyProfilePage.js
@@ -3,8 +3,6 @@ import HeaderPage from "./HeaderPage";
 class MyProfilePage {
   // LOCATORS
   elements = {
-    profileImage: () => HeaderPage.elements.profileImg(),
-    myProfile: () => HeaderPage.elements.dropdown.myProfile(),
     additionalHelp: () => cy.contains("Ayuda adicional"),
     profileHover: () => cy.get('[src="lib/octicons/build/svg/info-24.svg"]'), // there's no other option to correctly select this locator as of now
     back: () => cy.contains("#retorno", "Volver"),
@@ -17,41 +15,43 @@ class MyProfilePage {
       blog: () => cy.get(".socials-profile").find('[title="Blog"]'),
     },
 
-    user: () => cy.get('[title="Usuario"]'),
-    name: () => cy.get('[title="Nombre"]'),
-    firstSurname: () => cy.get('[title="Apellido 1"]'),
-    secondSurname: () => cy.get('[title="Apellido 2"]'),
-    email: () => cy.get('[title="Email"]'),
-    password: () => cy.get('[title="Contraseña"]'),
-    phone: () => cy.get('[title="Teléfono"]'),
-    photo: () => cy.get("#p_foto").find("input"),
-    documentType: () => cy.get(".selectpickerbtn").eq(0), // there's no other option as of now to get these locators
-    documentID: () => cy.get('[title="ID documento"]'),
-    birthDate: () => cy.get('[title="F. nacimiento"]'),
-    language: () => cy.get(".selectpickerbtn").eq(1),
-    gender: () => cy.get(".selectpickerbtn").eq(2),
-    education: () => cy.get('[title="Formación"]'),
-    company: () => cy.get('[title="Empresa"]'),
-    occupation: () => cy.get('[title="Puesto"]'),
-    CV: () => cy.get("#p_adjuntarCV").find("input"),
-    skype: () => cy.get('[title="Skype"]'),
-    facebook: () => cy.get('[title="Facebook"]'),
-    twitter: () => cy.get('[title="Twitter"]'),
-    linkedin: () => cy.get('[title="LinkedIn"]'),
-    blog: () => cy.get('[title="Blog"]'),
-    checkboxNoEmails: () => cy.get("#checkbox51"),
-    checkboxNotifications: () => cy.get("#checkbox52"),
-    checkboxNoGroupListing: () => cy.get("#checkbox53"),
-    checkboxAgendaEvents: () => cy.get("#checkbox54"),
-    observationsTextArea: () => cy.get("#observacionesPerfil"),
-    buttonActualizar: () => cy.get("#guardarPerfil"),
-    buttonCancelar: () => cy.get("#cancelarPerfilAlumno"),
+    form: {
+      user: () => cy.get('[title="Usuario"]'),
+      name: () => cy.get('[title="Nombre"]'),
+      firstSurname: () => cy.get('[title="Apellido 1"]'),
+      secondSurname: () => cy.get('[title="Apellido 2"]'),
+      email: () => cy.get('[title="Email"]'),
+      password: () => cy.get('[title="Contraseña"]'),
+      phone: () => cy.get('[title="Teléfono"]'),
+      photo: () => cy.get("#p_foto").find("input"),
+      documentType: () => cy.get(".selectpickerbtn").eq(0), // there's no other option as of now to get these locators
+      documentID: () => cy.get('[title="ID documento"]'),
+      birthDate: () => cy.get('[title="F. nacimiento"]'),
+      language: () => cy.get(".selectpickerbtn").eq(1),
+      gender: () => cy.get(".selectpickerbtn").eq(2),
+      education: () => cy.get('[title="Formación"]'),
+      company: () => cy.get('[title="Empresa"]'),
+      occupation: () => cy.get('[title="Puesto"]'),
+      CV: () => cy.get("#p_adjuntarCV").find("input"),
+      skype: () => cy.get('[title="Skype"]'),
+      facebook: () => cy.get('[title="Facebook"]'),
+      twitter: () => cy.get('[title="Twitter"]'),
+      linkedin: () => cy.get('[title="LinkedIn"]'),
+      blog: () => cy.get('[title="Blog"]'),
+      checkboxNoEmails: () => cy.get("#checkbox51"),
+      checkboxNotifications: () => cy.get("#checkbox52"),
+      checkboxNoGroupListing: () => cy.get("#checkbox53"),
+      checkboxAgendaEvents: () => cy.get("#checkbox54"),
+      observationsTextArea: () => cy.get("#observacionesPerfil"),
+      buttonActualizar: () => cy.get("#guardarPerfil"),
+      buttonCancelar: () => cy.get("#cancelarPerfilAlumno"),
+    }
   };
 
   // FUNCTIONS
 
-  // NO SE PUEDE ACCEDER A LA PAGINA /mi-perfil DESDE CYPRESS
-  // PAGEOBJECT Y ARCHIVO DE TEST editProfil.cy.js HECHO
+  // NO SE PUEDE ACCEDER A LA PAGINA '/mi-perfil' DESDE CYPRESS
+  // PAGEOBJECT Y ARCHIVO DE TEST 'editProfile.cy.js' HECHO
 
 }
 

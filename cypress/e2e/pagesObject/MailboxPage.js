59f81ab165155c50442f43cea677c0ce0a383a80
Juan-Claudio <angeelric@gmail.com>
2023-03-27 14:49:32 +0200

    Implement of Page Object of mailbox page

---
+++ cypress/e2e/pagesObject/MailboxPage.js
@@ -0,0 +1,32 @@
+class MailboxPage
+{
+    elements = {
+        breadCrumb: {
+            home:()=>cy.get('.breadcrumb > :nth-child(1) > .link'),
+            group:()=>cy.get('#grupo'),
+            active:()=>cy.get('.breadcrumb > .active')
+        },
+        menu:{
+            compose: ()=>{ cy.get('.sidebar-email-first-block') },
+            inbox: ()=>{ cy.get('#carpetas').eq(0) },
+            sent: ()=>{ cy.get('#carpetas').eq(1) },
+            trash: ()=>{ cy.get('#carpetas').eq(2) },
+            newFolder: ()=>{ cy.get('.row > [title="Nueva carpeta"]') },
+            manageFolders: ()=>{ cy.get('.row > [title="Gestionar carpetas"]') },
+            createdFolder: (name)=>{cy.get(`#carpetas img[title="${name}"]`).parent()}
+        },
+        searchInput: ()=>{ cy.get('#buscarCorreo') },
+        searchBtn: ()=>{ cy.get('#fBusquedaCorreo button') },
+        refreshIcon: ()=>{ cy.get('[title="Recargar"]') },
+        moveAtIcon: ()=>{ cy.get('.row > :nth-child(1) > [title="Mover a..."]') },
+        trashIcon: ()=>{ cy.get(':nth-child(1) > [title="Eliminar"]') },
+        markAsReadIcon: ()=>{ cy.get('[title="Marcar como leído"]') },
+        markAsUnreadIcon: ()=>{ cy.get('[title="Marcar como no leído"]') },
+        selectAllCheckbox: ()=>{ cy.get('#table [name="btSelectAll"]') },
+        mailList:{
+            checkbox_nb: (nb)=>{ return cy.get('#table > tbody > tr').eq(nb).find('.bs-checkbox > input') },
+            mail_nb: (nb)=>{ return cy.get('#table > tbody > tr').eq(nb) }
+        }
+    }
+}
+export default new MailboxPage;

23d37dc88b4c33f8fad4294093ad3fd47a99b0d9
Marcdev89 <marc.vitoria89@gmail.com>
2023-04-11 10:21:25 +0200

    New Test: Delete MF

---
+++ cypress/e2e/tests/deleteMF.cy.js
@@ -0,0 +1,30 @@
+import LoginPage from "../pagesObject/LoginPage";
+import user from "../../fixtures/userTypes.json"
+import DashboardPage from "../pagesObject/DashboardPage";
+import MMFFPage from "../pagesObject/MMFFPage";
+import NewMFPage from "../pagesObject/NewMFPage";
+
+    beforeEach(() => {
+        LoginPage.login(user.admin.name,user.admin.pass)
+        DashboardPage.elements.trainingManagementCard.modules().click()       
+    });
+
+it('Deleting a MF as a Administrator', () => {
+    //creating a MF first of all
+   MMFFPage.elements.buttons.newMF().click()
+   cy.url().should('include','nuevo-modulo-formativo')
+   NewMFPage.createNewMF('123test','Required',3)
+   cy.alertAssert('Módulo guardado correctamente')
+   //Searching and deleting the MF
+   MMFFPage.url()
+   MMFFPage.deleteMF(0)
+   MMFFPage.elements.tableList(0,1).should('not.have.value', '123test')  
+});  
+
+it('Trying to delete a MF but it belongs to AF', () => {
+    MMFFPage.elements.searchCard.btnShowAll().click()
+    //Need a complete AF (Arca de Noé)
+    cy.contains('Spam').siblings('.td-actions').find('[title="Eliminar"]').click()
+    MMFFPage.elements.modalConfirm.btnYes().click()
+    cy.get('#cuerpoModal').should('have.text', 'El módulo formativo no puede estar asignado a ninguna acción formativa')
+});

c6b02c4da9325c33a5288da4af6f9349258c8e1b
Marcdev89 <marc.vitoria89@gmail.com>
2023-04-20 10:03:29 +0200

    Fix: change data (arca de noé) and selectors

--- cypress/e2e/tests/showProfileList.cy.js
+++ cypress/e2e/tests/showProfileList.cy.js
@@ -5,7 +5,7 @@ import usersLogin from "../../fixtures/usersLogin.json"
 
 it('as a Administrator, show profile list from dashboard page', () => {
     LoginPage.login(userTypes.admin.name, userTypes.admin.pass)
-    DashboardPage.body.administrationCard.profiles().click()
+    DashboardPage.elements.administrationCard.profiles().click()
     cy.url().should('include', 'perfiles')
     usersLogin.map((e)=>{
         e.expected === "Superadministrador" ? cy.contains('Superadmin') : cy.contains(e.expected)
